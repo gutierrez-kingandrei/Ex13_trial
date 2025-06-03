508a2884c600feed9868521334b0a3521d5994af
condino-ciaramarie <ciaramariecondino0901@gmail.com>
2025-06-03 21:28:42 +0800

    Add condino module

---
+++ teamfive/condino.py
@@ -0,0 +1,41 @@
+import os
+def pause():
+    input("\nPress Enter to continue...")
+    
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def condino_menu():
+    while True:
+        clear_screen()
+        print("Hello Everyone! I am Ciara Marie Condino")
+        print("1. Enter Basic Info")
+        print("2. Enter Goals")
+        print("3. Comment from Arguelles")
+        print("4. Comment from Caya")
+        print("5. Comment from Cordova")
+        print("6. Comment from Gutierrez")
+        print("7. Exit")
+            
+        choice = input("\nSelect a team member: ")
+            
+        match choice:
+            case "1":
+                print("I am Ciara Marie Condino and I am 21 years old")
+                print("I love playing Grow a Garden in Roblox")
+                pause()
+            case "2":
+                print("My goal is to be a successful daughter and to be rich")
+                pause()
+            case "3":
+                pause()
+            case "4":
+                pause()
+            case "5":
+                pause()
+            case "6":
+                pause()
+            case "7":
+                break
+            case _:
+                print("Invalid choice. Please try again.")
